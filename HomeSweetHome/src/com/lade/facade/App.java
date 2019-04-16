92f535e3b0c52a464b8f04cbf178ffb39d72a17a
71148 <71148@CEUEKTIUNIV-052.ue.poznan>
2019-04-16 17:33:56 +0200

    Hello

--- HomeSweetHome/src/com/lade/facade/App.java
+++ HomeSweetHome/src/com/lade/facade/App.java
@@ -2,7 +2,8 @@ package com.lade.facade;
 
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Start");		
+		System.out.println("Start");	
+		System.out.println("Start");
 	}
 
 }
